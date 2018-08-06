3964179237ceef48f3b3a03f418c5277a9a85d66
Naing Tun <naing08@gmail.com>
2018-08-06 23:25:31 +0630

    UserLogin

--- server/security/auth.js
+++ server/security/auth.js
@@ -157,7 +157,7 @@ passport.use('cookie-site',new CustomStrategy(
           const sessionData = await verifySession(req.cookies.access_token);
           if(sessionData. error)
             done(null,{isAuthenticated:false});
-          else if(sessionData && sessionData.account_type !== "CUSTOMER"){
+          else if(sessionData && sessionData.account_type.toUpperCase() !== "CUSTOMER"){
             done(null,false);//if not customer force to login
           }else 
             done(null,sessionData);
